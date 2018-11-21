31a6a61d38b44896b8c087277153f72170c8dae6
Gábor Kovács <gabor950601@gmail.com>
2018-11-21 18:13:09 +0100

    clean code again

--- src/com/codecool/OptimusPrime.java
+++ src/com/codecool/OptimusPrime.java
@@ -45,10 +45,8 @@ public class OptimusPrime {
 
     private boolean isPrime(Integer num){
         boolean flag = true;
-        for(int i = 2; i <= num/2; ++i)
-        {
-            if(num % i == 0)
-            {
+        for(int i = 2; i <= num/2; ++i) {
+            if(num % i == 0) {
                 flag = false;
                 break;
             }
